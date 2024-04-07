20a48e51cf4957166123737964aa964cdde6fffe
miwanoff <marina.iva.noffa@gmail.com>
2024-04-07 12:26:42 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import booksData from "./books.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Hello() {
+  return (
+    <div>
+      <h1 style={{ color: "red" }}>Hello, world!</h1>
+      <h2>{booksData[0].name}</h2>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.createRoot(document.getElementById("root")).render(<Hello />);
