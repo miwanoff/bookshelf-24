4139649740120f5b0be6d8bb1bfb8a0289a53fa4
miwanoff <marina.iva.noffa@gmail.com>
2024-04-14 13:26:26 +0300

    Cange classes on hooks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import booksData from "./books.js";
 import logo from "./logo.svg";
@@ -40,64 +41,97 @@ class Count extends React.Component {
   }
 }
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      books: booksData,
-      cart: this.getBookData().length ? this.getBookData() : [],
-      term: "",
-      isChecked: false,
-    };
-  }
+// Отримати данні з LocalStorage 111
+const getBookData = () => {
+  // return localStorage.getItem("books")
+  //   ? JSON.parse(localStorage.getItem("books"))
+  //   : [];
+  return localStorage.getItem("goods")
+    ? JSON.parse(localStorage.getItem("goods"))
+    : [];
+};
 
-  // Отримати данні з LocalStorage 111
-  getBookData = () => {
-    return localStorage.getItem("books")
-      ? JSON.parse(localStorage.getItem("books"))
-      : [];
-  };
+// Запиати данні в LocalStorage
+const setBookData = (o) => {
+  // localStorage.setItem("books", JSON.stringify(o));
+  localStorage.setItem("goods", JSON.stringify(o));
+};
 
-  // Запиати данні в LocalStorage
-  setBookData = (o) => {
-    localStorage.setItem("books", JSON.stringify(o));
-  };
+const App = () => {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     books: booksData,
+  //     cart: this.getBookData().length ? this.getBookData() : [],
+  //     term: "",
+  //     isChecked: false,
+  //   };
+  // }
+  const [books, setBooks] = useState(booksData);
+  const [cart, setCart] = useState(getBookData);
+  const [term, setTerm] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
 
-  removeBook = (book) => {
-    const updateBooks = this.state.books.filter(function (item) {
+  const removeBook = (book) => {
+    // const updateBooks = this.state.books.filter(function (item) {
+    //   return item.id !== book.id;
+    // });
+    // console.log(updateBooks);
+    // this.setState({
+    //   books: updateBooks,
+    // });
+    let goods = books;
+    const updateBooks = goods.filter(function (item) {
       return item.id !== book.id;
     });
-    console.log(updateBooks);
-    this.setState({
-      books: updateBooks,
-    });
+    setBooks(updateBooks);
   };
 
-  addBookToCart = (book) => {
-    const goods = this.state.cart;
-    if (!goods.includes(book)) goods.push(book);
-    else book.count++;
-    this.setState({
-      cart: goods,
-    });
-    this.setBookData(goods);
+  const addBookToCart = (book) => {
+    // const goods = this.state.cart;
+    // if (!goods.includes(book)) goods.push(book);
+    // else book.count++;
+    // this.setState({
+    //   cart: goods,
+    // });
+    // this.setBookData(goods);
+    let goods = [...cart];
+    goods.length && goods.includes(book) ? book.count++ : goods.push(book);
+    setBookData(goods);
+    setCart(goods);
   };
 
-  deleteBookFromCart = (book) => {
+  const deleteBookFromCart = (book) => {
+    // let goods;
+    // if (book.count === 1)
+    //   goods = this.state.cart.filter((item) => item.id !== book.id);
+    // else
+    //   goods = this.state.cart.filter((item) =>
+    //     item.id === book.id ? book.count-- : book.count
+    //   );
+    // this.setState({
+    //   cart: goods,
+    // });
+    // this.setBookData(goods);
     let goods;
-    if (book.count === 1)
-      goods = this.state.cart.filter((item) => item.id !== book.id);
-    else
-      goods = this.state.cart.filter((item) =>
+    if (book.count === 1) {
+      goods = cart.filter((item) => item.id !== book.id);
+    } else {
+      goods = cart.filter((item) =>
         item.id === book.id ? book.count-- : book.count
       );
-    this.setState({
-      cart: goods,
-    });
-    this.setBookData(goods);
+    }
+    setBookData(goods);
+    setCart(goods);
   };
 
-  searchBook = (items, term) => {
+  const searchBook = (items, term) => {
+    // if (term.trim().length === 0) {
+    //   return items;
+    // }
+    // return items.filter((item) => {
+    //   return item.name.indexOf(term) > -1;
+    // });
     if (term.trim().length === 0) {
       return items;
     }
@@ -106,105 +140,114 @@ class App extends React.Component {
     });
   };
 
-  onUpdateSearch = (term) => {
-    this.setState({ term: term });
+  const onUpdateSearch = (term) => {
+    // this.setState({ term: term });
+    setTerm(term);
   };
 
-  sortBook = (items, isChecked) => {
+  const sortBook = (items, isChecked) => {
+    // if (isChecked) {
+    //   return items.sort((a, b) =>
+    //     a.name < b.name ? -1 : a.name === b.name ? 0 : 1
+    //   );
+    // } else {
+    //   return items.sort((a, b) => (a.id < b.id ? -1 : a.id === b.id ? 0 : 1));
+    // }
     if (isChecked) {
       return items.sort((a, b) =>
         a.name < b.name ? -1 : a.name === b.name ? 0 : 1
       );
     } else {
-      return items.sort((a, b) => (a.id < b.id ? -1 : a.id === b.id ? 0 : 1));
+      return items.sort((a, b) => a.id - b.id);
     }
   };
 
-  onUpdateSort = (isChecked) => {
-    this.setState({ isChecked: isChecked });
+  const onUpdateSort = (isChecked) => {
+    //this.setState({ isChecked: isChecked });
+    setIsChecked(isChecked);
   };
 
-  render() {
-    const { books, cart, term,  isChecked} = this.state;
-    const visibleBooks = this.searchBook(this.sortBook(
-      books,
-      isChecked
-    ), term);
-    //const visibleBooks = 
+  //render() {
+  // const { books, cart, term,  isChecked} = this.state;
+  // const visibleBooks = this.searchBook(this.sortBook(
+  //   books,
+  //   isChecked
+  // ), term);
+  //const visibleBooks =
+  const visibleBooks = searchBook(sortBook(books, isChecked), term);
+  return (
+    <div>
+      <Header className="container-fluid p-5 bg-dark text-primary text-center" />
 
-    return (
-      <div>
-        <Header className="container-fluid p-5 bg-dark text-primary text-center" />
-
-        <div className="container-fluid text-center">
-          <div className="row">
-            <div className="search-panel col-3 my-3">
-              <SearchPanel onUpdateSearch={this.onUpdateSearch} />
-            </div>
+      <div className="container-fluid text-center">
+        <div className="row">
+          <div className="search-panel col-3 my-3">
+            <SearchPanel onUpdateSearch={onUpdateSearch} />
           </div>
-          <div className="row">
-            <div className="col-3 my-3">
-              <SortPanel onUpdateSort={this.onUpdateSort}/>
-            </div>
-          </div>
-
-          <div className="row justify-content-center">
-            {visibleBooks.map((book) => {
-              // console.log(book.id);
-              return (
-                <div className="col-sm-4 col-12" key={book.id}>
-                  <div className="card text-center my-5 p-3">
-                    <BookItem
-                      book={book}
-                      removeBook={this.removeBook}
-                      addBookToCart={this.addBookToCart}
-                    />
-                  </div>
-                </div>
-              );
-            })}
+        </div>
+        <div className="row">
+          <div className="col-3 my-3">
+            <SortPanel onUpdateSort={onUpdateSort} />
           </div>
         </div>
 
-        <div className="container-fluid text-center">
-          <h4>Кошик товарів</h4>
-          <p>Кількість книг: {cart.length} </p>
-          <ul className="list-group">
-            {cart.map((book) => (
-              <li key={book.id} className="list-group-item">
-                <div className="row">
-                  <div className="col-3">{book.name}</div>
-                  <div className="col-3">{book.author}</div>
-                  <div className="col-2">{book.price}</div>
-                  <div className="col-1">{book.count}</div>
-                  <div className="col-3">
-                    <button
-                      onClick={this.deleteBookFromCart.bind(this, book)}
-                      type="button"
-                      className="btn btn-outline-primary mt-auto mb-2"
-                    >
-                      Delete
-                    </button>
-                  </div>
+        <div className="row justify-content-center">
+          {visibleBooks.map((book) => {
+            // console.log(book.id);
+            return (
+              <div className="col-sm-4 col-12" key={book.id}>
+                <div className="card text-center my-5 p-3">
+                  <BookItem
+                    book={book}
+                    removeBook={removeBook}
+                    addBookToCart={addBookToCart}
+                  />
                 </div>
-              </li>
-            ))}
-          </ul>
-          <div className="row">
-            <div className="col-12">
-              <Count goods={cart} />
-            </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="container-fluid text-center">
+        <h4>Кошик товарів</h4>
+        <p>Кількість книг: {cart.length} </p>
+        <ul className="list-group">
+          {cart.map((book) => (
+            <li key={book.id} className="list-group-item">
+              <div className="row">
+                <div className="col-3">{book.name}</div>
+                <div className="col-3">{book.author}</div>
+                <div className="col-2">{book.price}</div>
+                <div className="col-1">{book.count}</div>
+                <div className="col-3">
+                  <button
+                    onClick={() => deleteBookFromCart(book)}
+                    type="button"
+                    className="btn btn-outline-primary mt-auto mb-2"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div className="row">
+          <div className="col-12">
+            <Count goods={cart} />
           </div>
-          <div className="row">
-            <div className="col-12">
-              <Sum goods={cart} />
-            </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <Sum goods={cart} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+  //}
+};
 
 // function Image(props) {
 //   return <img src={props.src} alt="logo" style={{ width: "150px" }} />;
